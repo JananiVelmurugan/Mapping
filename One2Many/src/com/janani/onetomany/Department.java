04f2e57f7c46a9abdb158b60be4e016b8ab7c4e4
Janani <jananise@gmail.com>
2016-12-27 11:02:31 +0530

    One to Many

---
+++ One2Many/src/com/janani/onetomany/Department.java
@@ -0,0 +1,39 @@
+package com.janani.onetomany;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Department")
+public class Department {
+
+	@Id
+	@GeneratedValue
+	private int id;
+	private String name;
+
+	@OneToMany(mappedBy = "department")
+	private Set<Employee> employees;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ One2Many/src/com/janani/onetomany/Employee.java
@@ -0,0 +1,49 @@
+package com.janani.onetomany;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "empno")
+	private int empno;
+
+	private String username;
+
+	@ManyToOne
+	@JoinColumn(name = "department")
+	private Department department;
+
+	public int getEmpno() {
+		return empno;
+	}
+
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+}
